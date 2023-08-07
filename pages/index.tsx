67b85bebac1d6822fa1b692929c0a7e4883e9e03
fuuron <koki1016jp@gmail.com>
2023-08-08 02:01:35 +0900

    猫画像ジェネレーター

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
     return (
         <div className={styles.page}>
             <button onClick={handleClick} className={styles.button}>
-                One more cat!
+                One more cat!!
             </button>
             <div className={styles.frame}>
                 {loading || <img src={imageUrl}/>}
